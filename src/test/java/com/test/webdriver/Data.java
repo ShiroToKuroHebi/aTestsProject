6919afec9c9e613b6a52476b6c99f13dbf32f2ae
ShiroToKuroHebi <p.ds.sergeyev@gmail.com>
2017-12-20 10:45:55 +0300

    upd.

---
+++ src/test/java/com/test/webdriver/Data.java
@@ -0,0 +1,9 @@
+package com.test.webdriver;
+
+public interface Data {
+    //Authentication
+    String clientLogin = "1111111111";
+    String clientPswrd = "1111111111";
+    
+    
+}
--- src/test/java/com/test/webdriver/test_000_start.java
+++ src/test/java/com/test/webdriver/test_000_start.java
@@ -5,51 +5,50 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class test_000_start {
 
     private WebDriver chrome = new ChromeDriver();
 
     @Test
-    public void goToStand(){
+    public void goToStand() {
         chrome.get("http://stand.vtb.jtcc.ru:16006/");
         assertEquals("VTB DBO front", chrome.getTitle());
         chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
-    public void logIn(){
+    public void logIn() {
         WebElement loginField = chrome.findElement(By.xpath("//input[@placeholder='Логин']"));
         WebElement passwordField = chrome.findElement(By.xpath("//input[@type='password']"));
         WebElement buttonLogIn = chrome.findElement(By.xpath("//button[text()='Войти']"));
 
         loginField.clear();
-        loginField.sendKeys("1111111111");
+        loginField.sendKeys(Data.clientLogin);
         passwordField.clear();
-        passwordField.sendKeys("1111111111");
+        passwordField.sendKeys(Data.clientPswrd);
         buttonLogIn.click();
     }
 
     @Test
-    public void createRP(){
+    public void createRP() {
         WebElement buttonCreate = chrome.findElement(By.xpath(".//*[text()='Создать ПП']/.."));
 
         buttonCreate.click();
     }
 
     @Test
-    public void allInOne(){
+    public void makeItHappen() {
         goToStand();
         logIn();
-        chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         createRP();
+        
+        
+        
         //chrome.quit();
     }
 }
